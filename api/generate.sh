8fc26c580ed42d8cef6bd85c5dc6dff438a4113a
Karl Cardenas <karl@spectrocloud.com>
2025-12-18 16:51:39 -0700

    ci: fix cd logic

--- api/generate.sh
+++ api/generate.sh
@@ -6,11 +6,14 @@
 set -e
 
 # Fetch the latest hapi spec
-rm -rf hapi && (git clone git@github.com:spectrocloud/hapi.git || git clone https://github.com/spectrocloud/hapi)
+if [ -d "hapi" ]; then
+    (cd hapi && git pull origin master --ff)
+else
+    git clone git@github.com:spectrocloud/hapi.git || git clone https://github.com/spectrocloud/hapi
+fi
 (
     cd hapi
     bash generate_hubble_spec.sh
     go run api/main.go
     cp gen/docs-spec/palette-apis-spec.json ..
-    rm -rf hapi
 )
